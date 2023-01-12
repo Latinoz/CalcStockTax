2747df1b94efe496b4b471faacee9fa9389eaab8
Себастьян Перейро <m4r4d3r@hotmail.com>
2023-01-12 17:05:10 +0400

    Small fix

--- CalcStockTax/DbSRV/Program.cs
+++ CalcStockTax/DbSRV/Program.cs
@@ -1,15 +1,7 @@
 using DbSRV.DB;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
-
-//builder.Services.AddDbContext<ApplicationContext>(options =>
-//{
-//    var connectionString = builder.Configuration.GetConnectionString("MariaDbConnectionString");
-//    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
-//});
 
 builder.Services.AddDbContextPool<ApplicationContext>(options =>
 {
@@ -19,7 +11,5 @@ builder.Services.AddDbContextPool<ApplicationContext>(options =>
 
 
 var app = builder.Build();
-
-//app.MapGet("/", () => "Hello World!");
 
 app.Run();
