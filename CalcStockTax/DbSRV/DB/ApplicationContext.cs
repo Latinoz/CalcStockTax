360df1632c5508aae7d86a50de66e101d511e087
Себастьян Перейро <m4r4d3r@hotmail.com>
2023-01-12 12:44:41 +0400

    Modify Db connect

---
+++ CalcStockTax/DbSRV/DB/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using DbSRV.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DbSRV.DB
+{
+    public class ApplicationContext : DbContext
+    {
+        //public ApplicationContext() => Database.EnsureCreated();
+        //
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+        : base(options)
+        {
+            //Database.EnsureCreated();
+        }
+
+        public DbSet<Investment> Investments => Set<Investment>();
+        public DbSet<Tariff> Tariffs => Set<Tariff>();
+        public DbSet<Tax> Taxs => Set<Tax>();
+        public DbSet<TaxAmount> TaxAmounts => Set<TaxAmount>();
+    }
+}
--- CalcStockTax/DbSRV/Program.cs
+++ CalcStockTax/DbSRV/Program.cs
@@ -1,4 +1,14 @@
+using DbSRV.DB;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContextPool<ApplicationContext>(options => options
+        .UseMySql(builder.Configuration.GetConnectionString("MariaDbConnectionString")
+        ,ServerVersion.AutoDetect("MariaDbConnectionString")));
+
 var app = builder.Build();
 
 //app.MapGet("/", () => "Hello World!");
